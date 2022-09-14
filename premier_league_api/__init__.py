109ed711cfb3dc5af17ec92f7d5b2b10e9e67353
Jacob White <jake@jwhite.network>
2022-09-13 22:29:39 -0400

    Bump version

--- premier_league_api/__init__.py
+++ premier_league_api/__init__.py
@@ -4,6 +4,6 @@ from .meta_competitions import create_competitions_file
 from .errors import EmptyResponse
 from .competitions import *
 
-__version__ = "1.1.0"
+__version__ = "2.0.0"
 
 __all__ = ["APIClient", "__version__", "EmptyResponse", "create_competitions_file"]
